01081e69af3d5d3533224f363330c3cd0527ade2
sheka <sheka@tcs.com>
2024-09-14 23:48:47 +0530

    pom.xml

---
+++ src/test/java/hub/example/hl/HlApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.example.hl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
